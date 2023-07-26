1e7ea544ffa2189999a5fb2577fbf7c87fc55fb8
jack <speideljackson@protonmail.com>
2023-07-26 13:54:32 -0400

    many changes to many things - unfocused additions/edits

---
+++ src/components/MainCards/MainCards.jsx
@@ -0,0 +1,14 @@
+import './mainCards.scss';
+
+const MainCards = () => {
+  return (
+    <div className='cards'>
+        <ul className='cards__list'>
+            <li className='cards__list-item'>articles</li>
+            <li className='cards__list-item'>resources</li>
+        </ul>
+    </div>
+  )
+}
+
+export default MainCards;
