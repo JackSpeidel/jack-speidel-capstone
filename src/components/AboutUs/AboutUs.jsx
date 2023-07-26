a100076268310616040f5b957a59601ab2ec764c
jack <speideljackson@protonmail.com>
2023-07-26 16:58:52 -0400

    setup about-us

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,13 +1,16 @@
 import './aboutUs.scss'
 
 const AboutUs = () => {
-  return (
-    <div className='about'>
-        <section className='about__heading'>
-            <h2>Who We Are</h2>
-        </section>
-    </div>
-  )
+	return (
+		<div className='about'>
+			<section className='about__heading'>
+				<h2>Who We Are</h2>
+			</section>
+			<section className='about__body'>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, quisquam aspernatur! Nam iusto, obcaecati animi aspernatur praesentium voluptate eius dolorum. Laudantium amet non cum odit mollitia inventore illum omnis magnam!</p>
+			</section>
+		</div>
+	)
 }
 
 export default AboutUs;
